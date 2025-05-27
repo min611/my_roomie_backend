ce1636095566caa4cd64c5b256bd0fae7e144b6a
min611 <net678600@gmail.com>
2025-05-28 00:03:57 +0900

    Update RoommateSurveyResponseDto.java
    (수면, 기상, 전화, 샤워) 시간 추가

--- src/main/java/com/everytime/Hackathon2025/Dto/RoommateSurveyResponseDto.java
+++ src/main/java/com/everytime/Hackathon2025/Dto/RoommateSurveyResponseDto.java
@@ -3,8 +3,6 @@ package com.everytime.Hackathon2025.Dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @AllArgsConstructor
 public class RoommateSurveyResponseDto {
@@ -14,7 +12,12 @@ public class RoommateSurveyResponseDto {
     private String cleanLevel;
     private boolean smoking;
     private String etc;
+
+    private String wakeUpTime;
+    private String sleepTime;
+    private String showerTime;
+    private String callTime;
+
     private String createdAt;
     private boolean isMine;
-
 }
