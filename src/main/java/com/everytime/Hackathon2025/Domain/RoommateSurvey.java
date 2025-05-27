23a5a4d67ceb826fab697eab0137bcfd3b7dc28f
min611 <net678600@gmail.com>
2025-05-28 00:00:26 +0900

    Update RoommateSurvey.java
    (취침, 기상, 샤워, 전화) 시간 추가

--- src/main/java/com/everytime/Hackathon2025/Domain/RoommateSurvey.java
+++ src/main/java/com/everytime/Hackathon2025/Domain/RoommateSurvey.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-
 @Entity
 @NoArgsConstructor
 @Getter
@@ -29,12 +28,25 @@ public class RoommateSurvey {
     @Column(nullable = false)
     private boolean smoking;
 
-    @Column(length = 500) //기타
+    @Column(length = 500)
     private String etc;
+
+    @Column(name = "wake_up_time")
+    private String wakeUpTime;
+
+    @Column(name = "sleep_time")
+    private String sleepTime;
+
+    @Column(name = "shower_time")
+    private String showerTime;
+
+    @Column(name = "call_time")
+    private String callTime;
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
+    // 기본 설문 생성자 (시간 없이)
     public RoommateSurvey(User user, String dormName, String cleanLevel, boolean smoking, String etc) {
         this.user = user;
         this.dormName = dormName;
@@ -42,7 +54,20 @@ public class RoommateSurvey {
         this.smoking = smoking;
         this.etc = etc;
         this.createdAt = LocalDateTime.now();
-
     }
 
+    // 확장 설문 생성자 (시간 포함)
+    public RoommateSurvey(User user, String dormName, String cleanLevel, boolean smoking, String etc,
+                          String wakeUpTime, String sleepTime, String showerTime, String callTime) {
+        this.user = user;
+        this.dormName = dormName;
+        this.cleanLevel = cleanLevel;
+        this.smoking = smoking;
+        this.etc = etc;
+        this.wakeUpTime = wakeUpTime;
+        this.sleepTime = sleepTime;
+        this.showerTime = showerTime;
+        this.callTime = callTime;
+        this.createdAt = LocalDateTime.now();
+    }
 }
