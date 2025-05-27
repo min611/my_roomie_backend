8151b82c1a5db870f936b84b7a66871d0443dc07
min611 <net678600@gmail.com>
2025-05-27 23:56:51 +0900

    Update RoommateSurvey.java

--- src/main/java/com/everytime/Hackathon2025/Domain/RoommateSurvey.java
+++ src/main/java/com/everytime/Hackathon2025/Domain/RoommateSurvey.java
@@ -35,14 +35,30 @@ public class RoommateSurvey {
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    public RoommateSurvey(User user, String dormName, String cleanLevel, boolean smoking, String etc) {
-        this.user = user;
-        this.dormName = dormName;
-        this.cleanLevel = cleanLevel;
-        this.smoking = smoking;
-        this.etc = etc;
-        this.createdAt = LocalDateTime.now();
+    @Column(name = "wake_up_time")
+    private String wakeUpTime;
 
-    }
+    @Column(name = "sleep_time")
+    private String sleepTime;
 
+    @Column(name = "shower_time")
+    private String showerTime;
+
+    @Column(name = "call_time")
+    private String callTime;
+
+
+   public RoommateSurvey(User user, String dormName, String cleanLevel, boolean smoking, String etc,
+                      String wakeUpTime, String sleepTime, String showerTime, String callTime) {
+    this.user = user;
+    this.dormName = dormName;
+    this.cleanLevel = cleanLevel;
+    this.smoking = smoking;
+    this.etc = etc;
+    this.wakeUpTime = wakeUpTime;
+    this.sleepTime = sleepTime;
+    this.showerTime = showerTime;
+    this.callTime = callTime;
+    this.createdAt = LocalDateTime.now();
+  }
 }
